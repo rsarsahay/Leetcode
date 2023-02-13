54d6bdfeb7d8706864ef647e9df701529e85a9b4
Kumari Ritu Sarsahay <102847337+rsarsahay@users.noreply.github.com>
2023-02-13 14:05:53 +0530

    Add files via upload

---
+++ Array/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,27 @@
+//Intuition
+/*We will linearly travel the array. 
+We can maintain a minimum from the start of the array
+and compare it with every element of the array, if it is
+greater than the minimum then take the difference and maintain it in max, 
+otherwise update the minimum.*/
+
+//Time complexity: O(n)
+//Space complexity: O(1)
+
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_pro=0;
+        int n=prices.size();
+        int min_price=INT_MAX;
+
+        for(int i=0; i<n; i++)
+        {
+            min_price=min(min_price, prices[i]);
+            max_pro=max(max_pro, prices[i]-min_price);
+        }
+
+        return max_pro;
+    }
+};
