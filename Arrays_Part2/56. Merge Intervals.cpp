5f3e872287a3fe4b1cadbcbc45fd7f58437315bf
Kumari Ritu Sarsahay <102847337+rsarsahay@users.noreply.github.com>
2023-02-15 14:32:25 +0530

    Update 56. Merge Intervals.cpp

--- Arrays_Part2/56. Merge Intervals.cpp
+++ Arrays_Part2/56. Merge Intervals.cpp
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/merge-intervals/
 
-/Intuition
+//Intuition
 /*Sort the intervals, the intervals which will be merging are 
 bound to be adjacent. We kept on merging simultaneously as we were traversing 
 through the array and when the element was non-overlapping we simply inserted 
