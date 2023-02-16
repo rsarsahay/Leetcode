11aff691ad5ebcf03937f777448e1227e875b136
Kumari Ritu Sarsahay <102847337+rsarsahay@users.noreply.github.com>
2023-02-16 14:14:20 +0530

    Update Repeat and Missing Number Array.cpp

--- Arrays_Part2/Repeat and Missing Number Array.cpp
+++ Arrays_Part2/Repeat and Missing Number Array.cpp
@@ -5,9 +5,9 @@ Intuition + Approach:
 
 Since the numbers are from 1 to N in the array arr[]
 
-1. Take a new_A array of size N+1 and initalize it with 0.
-2. Traverse the given array and increase the value of new_A[A[i]] by one .
-3. Then again traverse the new_A array starting from index 1 to N.
+1. Take a B vector of size N+1 and initalize it with 0.
+2. Traverse the given array and increase the value of B[A[i]] by one .
+3. Then again traverse the B array starting from index 1 to N.
 
 If you find any index value greater than 1 that is repeating element A.
 If you find any index value = 0 then that is the missing element B.
